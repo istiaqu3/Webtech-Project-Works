e2205a2ce5c4ffd1858fc55338336dc186d7b1a7
Mazid001 <39764711+Mazid001@users.noreply.github.com>
2018-08-05 15:50:54 +0600

    include headerin homep and headerL in userp
    homep=homepages, userp=userpages.
    header for login options, headerL for a/t/s infos and showing who is logged in with logout options

---
+++ include/header.php
@@ -0,0 +1,22 @@
+<table width="80%" align="center" cellspacing="0" cellpadding="10" border="1">
+    <tr>
+        <td valign="middle" height="70">  
+			<table width="100%">
+				<tr>					
+					<td>
+						<a href="home1.html" target="iFrame">
+							<img height="48" src="image/hi.png" />
+						</a>
+					</td>
+					<td align="right">
+						<a href="Dashboard.php" target="iFrame">Home</a>&nbsp;|
+						<a href="login.php" target="iFrame">Login</a></br>
+						<a href="Teacher_reg.php"><img src="image/login.png">Registration As Teacher</a>&nbsp;|
+						<a href="Student_reg.php"><img src="image/login.png">Registration As Student</a>
+					</td>
+				</tr>
+			</table>
+        </td>
+    </tr>
+    <tr>
+        <td align="center">
